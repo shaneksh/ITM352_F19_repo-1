0d32598e73fec20bc2d376bb347dae184daf6412
alyssatu <54603675+alyssatu@users.noreply.github.com>
2019-11-13 21:11:04 -1000

    Add files via upload

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -1,15 +1,21 @@
+// Source: Lab 13
+
+// initialize express
 var express = require('express');
 var app = express();
+
+// require to get data from body
 var myParser = require("body-parser");
 var products = require ("./public/products.js");
 const querystring = require('querystring');
 
-
+// enables a response 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
 
+// processes form to POST into invoice
 app.use(myParser.urlencoded({ extended: true }));
 app.post("/process_form", function (request, response) {
     let POST = request.body;
@@ -24,7 +30,8 @@ app.post("/process_form", function (request, response) {
             hasPurchases = true;
         }
     }
-    //if data is valid give user an invoice, if not give them an error
+    // valid data = invoice
+    // unvalid data = error
      qString = querystring.stringify (POST);
      if (hasValidQuantities == true && hasPurchases == true) {
        
@@ -34,28 +41,31 @@ app.post("/process_form", function (request, response) {
         response.redirect ("./products_page.html?" + qString); 
     }
 
-  
-    //cut and pasted from order_page copy.htmlf
+    // Source: Lab 13 order_page.html
     if (typeof POST['quantity_textbox'] != 'undefined') {
         displayPurchase(POST, response);
 
     }
 });
 
+// looks in public for response to GET
 app.use(express.static('./public'));
+
+// sets server to listen on localhost:8080
 app.listen(8080, () => console.log(`listening on port 8080`));
 
-//copied from order_page
+// Source: Lab 13 order_page.html
+/// validates string is a non-negative integer
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
-    if (q== '') {q=0};
-    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    return returnErrors ? errors : (errors.length == 0);
+    if (q== '') {q=0};  // for quantities not inputted
+    if (Number(q) != q) errors.push('Not a number!'); // checks for non-numbers
+    if (q < 0) errors.push('Negative value!'); // checks for negative numbers
+    if (parseInt(q) != q) errors.push('Not an integer!'); // checks for non-integers
+    return returnErrors ? errors : (errors.length == 0); // returns error if any
 
 }
-// cut and pasted from order_page copy
+// Source: Lab 13 order_page.html
 function displayPurchase(POST, response) {
 
     // Exercise 2b. Does not rewrite page if input is a NonNegInt
