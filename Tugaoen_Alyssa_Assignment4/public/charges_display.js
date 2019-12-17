5fb45ad4c44bd3aeaad3d3fa597620a5909c320d
alyssatu <54603675+alyssatu@users.noreply.github.com>
2019-12-16 21:04:20 -1000

    Add files via upload

---
+++ Tugaoen_Alyssa_Assignment4/public/charges_display.js
@@ -0,0 +1,37 @@
+charges =
+[
+  {  
+  "service": "Well visits and physicals",
+  "description": "White Sunflower Scrunchie",  
+  "price": 50.00  
+  },
+  {  
+  "service": "Immunizaiton",
+  "description": "Navy Sunflower Scrunchie", 
+  "price": 50.00 
+  },
+  {  
+    "service": "Nutrition and weight loss care",
+    "description": "Triangle Pearl Snap Clip",
+    "price": 100.00  
+  },
+  {  
+    "service": "Women's Health",
+    "description": "Basic women's healthcare including Pap smears for cervical cancer testing and treatment of PMS and migraine headaches.",  
+    "price": 125.00
+  },
+  {  
+    "service": "Acute illness and injuries",
+    "description": "2 Pack Crystal Snap Clips", 
+    "price": 75.00  
+  },
+  {  
+    "service": "Chronic illness",
+    "description": "2 Pack Pearl Snap Clips",
+    "price": 100.00
+  }
+]
+
+if (typeof module != 'undefined') {
+  module.exports = charges;
+} 
