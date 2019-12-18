fde6406d62efa29b29361c2c8cd2e93c17b68c9b
alyssatu <54603675+alyssatu@users.noreply.github.com>
2019-12-17 22:21:58 -1000

    Add files via upload

--- Tugaoen_Alyssa_Assignment4/server.js
+++ Tugaoen_Alyssa_Assignment4/server.js
@@ -9,7 +9,8 @@ app.use("/static", express.static('./public/'));
 
 app.use(myParser.urlencoded({ extended: true })); //tells the system to use JSON
 fs = require('fs'); //assigns to a fs variable
-var filename = './public/staff_data.json'; //assigns information from the user_registartion_info variable
+var filename = './public/staff_data.json'; //assigns information from the staff login info to variable
+var patientfile = './invoice_data.json'; // assigns invoice information to a variable
 
 // use middlewear 
 app.all('*', function (request, response, next) {
@@ -34,9 +35,22 @@ if (fs.statSync(filename)) {
 
     console.log(users_reg_data);
     //if file doesnt exist this will tell the user that it doesnt
+
 } else {
     console.log(filename + ' does not exist'); //shows the wrong file name + doesnt exist in the message
 }
+
+// checks to make sure 
+if (fs.statSync(patientfile)) {
+
+    //returns contents of the path
+    patient_data = fs.readFileSync(patientfile, 'utf-8');
+
+    stats = fs.statSync(patientfile);
+    console.log(patientfile + ' has ' + stats.size + ' characters'); //console logs the filename with the amount of characters it has
+    invoice_reg_data = JSON.parse(data);//parses the data into JSON format
+
+    console.log(users_reg_data);
 
 // validation for staff login
 app.post("./login", function (request, response) {
@@ -75,13 +89,37 @@ app.post("./login", function (request, response) {
     response.redirect("./staff.html");
 });
 
-// session ID for invoice to use later when patient logs in
-app.get('/use_session', function (request, response) {
-  response.send(`Welcome, your invoice ID is ${request.session.id}`)
+// puts invoice information into array
+// gets /invoice_register to make a form for staff to bill charges to specific invoice IDs
+app.get("/invoice_register", function (request, response) {
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="patientname" size="40" placeholder="Enter Patient Name" ><br />
+<input type="text" name="invoiceID" size="40" placeholder="Enter Invoice ID"><br />
+<input type="text" name="amount" size="40" placeholder="Enter Total Amount Due"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
 });
 
-// if server gets a POST request to login
-app.post("/login", function (request, response) {
-  if (typeof request.cookies.patientname != 'undefined') {
-      response.send(`Welcome back ${request.cookies.patientname}!);
-  }
+app.post("/invoice_register", function (request, response) {
+    // define patientname as a variable
+    patientname = request.body.patientname;
+    // empty object makes newuser a property of the array
+    invoice_reg_data[patientname] = {};
+    // invoiceID and amount
+    invoice_reg_data[patientname].invoiceID = request.body.invoiceID;
+    invoice_reg_data[patientname].amount = request.body.amount;
+
+    // adds the submitted form data to users_reg_data then saves this updated object to user_data.json
+    fs.writeFileSync(filename, JSON.stringify(invoice_reg_data));
+
+    // for assignment 2 send them to the invoice with the quanitity data and the username
+    response.send(`${patientname} charges billed to account!`);
+}
+console.log(request.body);
+
+});
